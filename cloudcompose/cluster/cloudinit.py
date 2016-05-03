c8e711ddeac9d9b9427430e2636cdee0d7f0fbd0
Patrick Cullen <patrick.cullen@washpost.com>
2016-05-03 15:48:51 -0400

    added overrides for docker-compose location

--- cloudcompose/cluster/cloudinit.py
+++ cloudcompose/cluster/cloudinit.py
@@ -1,6 +1,6 @@
 from cloudcompose.template import Template
 from cloudcompose.cluster.dockercompose import DockerCompose
-from os.path import join
+from os.path import join, split
 from pprint import pprint
 
 class CloudInit():
@@ -8,17 +8,17 @@ class CloudInit():
         self.cloud_config = cloud_config
 
     def build(self, **kwargs):
-        config_data, config_dir = self.cloud_config.config_data('cluster')
+        config_data = self.cloud_config.config_data('cluster')
 
         for key, value in kwargs.iteritems():
             config_data['_' + key] = value
 
-        template_dir = join(config_dir, 'templates')
+        template_dir, template_file = split(config_data['template'])
         self._add_docker_compose(template_dir, config_data)
-        return self._render_template(template_dir, config_data['template'], config_data)
+        return self._render_template(template_dir, template_file, config_data)
 
     def _add_docker_compose(self, template_dir, config_data):
-        docker_compose = DockerCompose()
+        docker_compose = DockerCompose(config_data['docker_compose'], config_data['docker_compose_override'])
         docker_compose, docker_compose_override = docker_compose.yaml_files(config_data)
         config_data['docker_compose'] = {}
         config_data['docker_compose']['yaml'] = docker_compose
--- cloudcompose/cluster/dockercompose.py
+++ cloudcompose/cluster/dockercompose.py
@@ -1,9 +1,11 @@
 from cloudcompose.template import Template
-from os.path import join, isfile, isdir
+from os.path import join, isfile, isdir, split
 
 class DockerCompose:
-    def __init__(self):
+    def __init__(self, docker_compose_path=None, docker_compose_override_path=None):
         self.config_dirs = ['cloud-compose', '.']
+        self.docker_compose_path = docker_compose_path
+        self.docker_compose_override_path = docker_compose_override_path
         self.docker_compose_files = ['docker-compose.yml', 'docker-compose.yaml']
         self.docker_compose_override_files = ['docker-compose.override.yml', 'docker-compose.override.yaml']
 
@@ -13,11 +15,17 @@ class DockerCompose:
         return docker_compose, docker_compose_override
 
     def _read_docker_compose(self):
+        if not self.docker_compose_path:
+            self.docker_compose_path = self._find_docker_compose_path()
+
+        return self._read_file(self.docker_compose_path)
+
+    def _find_docker_compose_path(self):
         for docker_compose_dir in ['.', '..']:
             for docker_compose_file in self.docker_compose_files:
                 docker_compose_path = join(docker_compose_dir, docker_compose_file)
                 if isfile(docker_compose_path):
-                    return self._read_file(docker_compose_path)
+                    return docker_compose_path
 
     def _read_file(self, file_path):
         with open(file_path, 'r') as f:
@@ -25,13 +33,19 @@ class DockerCompose:
         return contents
 
     def _render_docker_compose_override(self, config_data):
+        if not self.docker_compose_override_path:
+            self.docker_compose_override_path = self._find_docker_compose_override_path()
+        template_dir, template_file = split(self.docker_compose_override_path)
+        return self._render_template(template_dir, template_file, config_data)
+
+    def _find_docker_compose_override_path(self):
         for config_dir in self.config_dirs:
             template_dir = join(config_dir, 'templates')
             if isdir(template_dir):
                 for docker_compose_override_file in self.docker_compose_override_files:
                     template_file = join(template_dir, docker_compose_override_file)
                     if isfile(template_file):
-                        return self._render_template(template_dir, docker_compose_override_file, config_data)
+                        return template_file
 
     def _render_template(self, template_dir, template_file, template_data):
         template = Template(template_dir)
