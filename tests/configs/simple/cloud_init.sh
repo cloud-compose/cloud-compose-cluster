8992a7f2e2d625c66c6491b8045edbfcca1da97c
Patrick Cullen <patrick.cullen@washpost.com>
2016-05-03 20:03:50 -0400

    added simple example for unit tests

---
+++ tests/configs/simple/cloud_init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# system.mounts.sh
+mkfs -t ext4 /dev/xvdc
+echo -e '/dev/xvdc\t/data/mongodb\text4\tdefaults,noatime\t0\t0' >> /etc/fstab
+mkdir -p /data/mongodb
+mount /data/mongodb
+# docker_compose.run.sh 
+cat << EOF > /tmp/docker-compose.yml
+mongodb:
+  container_name: mongodb 
+  command: --shardsvr --replSet rs0 --dbpath /data/db
+  ports:
+    - "27018:27018"
+
+EOF
+cat << EOF > /tmp/docker-compose.override.yml
+mongodb:
+  image: washpost/mongodb:3.2
+  extra_hosts:
+    - "node0:10.0.10.10"
+  volumes:
+    - "/data/mongodb/mongodb:/data/db"
+EOF
