2622112f3fc345e96108e97b6602790901c96358
Patrick Cullen <patrick.cullen@washpost.com>
2016-05-12 10:21:08 -0400

    moved cloudinit common code to cloud-compose

--- cloudcompose/cluster/cloudinit.py
+++ cloudcompose/cluster/cloudinit.py
@@ -2,29 +2,18 @@ from cloudcompose.cluster.template import Template
 from cloudcompose.cluster.dockercompose import DockerCompose
 from os.path import join, split
 from pprint import pprint
+from cloudcompose.cloudinit import CloudInit as BaseCloudInit
 
-class CloudInit():
+class CloudInit(BaseCloudInit):
     def __init__(self, base_dir='.'):
-        self.base_dir = base_dir
-        self.template_file = 'cluster.sh'
+        BaseCloudInit.__init__(self, 'cluster', base_dir)
 
-    def build(self, config_data, **kwargs):
-        raw_search_path = config_data['search_path']
-        raw_search_path.insert(0, '.')
-        search_path = [join(self.base_dir, path) for path in raw_search_path]
-        for key, value in kwargs.iteritems():
-            config_data['_' + key] = value
+    def build_pre_hook(self, config_data, **kwargs):
+        self._add_docker_compose(config_data)
 
-        self._add_docker_compose(config_data, search_path)
-        return self._render_template(config_data, search_path)
-
-    def _add_docker_compose(self, config_data, search_path):
-        docker_compose = DockerCompose(search_path)
+    def _add_docker_compose(self, config_data):
+        docker_compose = DockerCompose(self.search_path(config_data))
         docker_compose, docker_compose_override = docker_compose.yaml_files(config_data)
         config_data['docker_compose'] = {}
         config_data['docker_compose']['yaml'] = docker_compose
         config_data['docker_compose']['override_yaml'] = docker_compose_override
-
-    def _render_template(self, template_data, search_path):
-        template = Template(search_path)
-        return template.render(self.template_file, template_data)
