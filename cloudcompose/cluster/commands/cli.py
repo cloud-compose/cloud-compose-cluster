c84360569bd4cfc4f0066660e624d0ea86fe9059
Patrick Cullen <patrick.cullen@washpost.com>
2016-06-10 19:30:04 -0400

    improved the error message for a missing file

--- cloudcompose/cluster/commands/cli.py
+++ cloudcompose/cluster/commands/cli.py
@@ -2,6 +2,7 @@ import click
 from cloudcompose.cluster.cloudinit import CloudInit
 from cloudcompose.cluster.aws.cloudcontroller import CloudController
 from cloudcompose.config import CloudConfig
+from cloudcompose.exceptions import CloudComposeException
 
 @click.group()
 def cli():
@@ -15,39 +16,51 @@ def up(cloud_init, use_snapshots, upgrade_image):
     """
     creates a new cluster
     """
-    cloud_config = CloudConfig()
-    ci = None
+    try:
+        cloud_config = CloudConfig()
+        ci = None
 
-    if cloud_init:
-        ci = CloudInit()
+        if cloud_init:
+            ci = CloudInit()
 
-    cloud_controller = CloudController(cloud_config)
-    cloud_controller.up(ci, use_snapshots, upgrade_image)
+        cloud_controller = CloudController(cloud_config)
+        cloud_controller.up(ci, use_snapshots, upgrade_image)
+    except CloudComposeException as ex:
+        print ex.message
 
 @cli.command()
 def down():
     """
     destroys an existing cluster
     """
-    cloud_config = CloudConfig()
-    cloud_controller = CloudController(cloud_config)
-    cloud_controller.down()
+    try:
+        cloud_config = CloudConfig()
+        cloud_controller = CloudController(cloud_config)
+        cloud_controller.down()
+    except CloudComposeException as ex:
+        print ex.message
 
 @cli.command()
 def cleanup():
     """
     deletes launch configs and auto scaling group
     """
-    cloud_config = CloudConfig()
-    cloud_controller = CloudController(cloud_config)
-    cloud_controller.cleanup()
+    try:
+        cloud_config = CloudConfig()
+        cloud_controller = CloudController(cloud_config)
+        cloud_controller.cleanup()
+    except CloudComposeException as ex:
+        print ex.message
 
 @cli.command()
 def build():
     """
     builds the cloud_init script
     """
-    cloud_config = CloudConfig()
-    config_data = cloud_config.config_data('cluster')
-    cloud_init = CloudInit()
-    print cloud_init.build(config_data)
+    try:
+        cloud_config = CloudConfig()
+        config_data = cloud_config.config_data('cluster')
+        cloud_init = CloudInit()
+        print cloud_init.build(config_data)
+    except CloudComposeException as ex:
+        print ex.message
